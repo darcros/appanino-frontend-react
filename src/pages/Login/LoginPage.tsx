f3092ac3868fb1151438542c6fa544845a129985
BuildTools <darcros7@gmail.com>
2019-07-24 13:16:45 +0200

    login page layout

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,15 +1,70 @@
 import React from 'react';
-import { Box, Typography } from '@material-ui/core';
+import { TextField, FormControlLabel, Button, Checkbox, makeStyles, Avatar, Typography } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
 import { PageContainer } from '../../components/PageContainer';
 
+const useStyles = makeStyles(theme => ({
+  container: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 export const LoginPage: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <PageContainer maxWidth="xl">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Login page
+    <PageContainer maxWidth="xs">
+      <div className={classes.container}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Login to Appanino
         </Typography>
-      </Box>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
+          <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+            Sign In
+          </Button>
+        </form>
+      </div>
     </PageContainer>
   );
 };
