dc1c43117eb1d151d3a0493da4387108cc70201f
BuildTools <darcros7@gmail.com>
2019-08-28 20:50:16 +0200

    display loading idicator correctly

--- src/components/LoadingButton.tsx
+++ src/components/LoadingButton.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { makeStyles, CircularProgress } from '@material-ui/core';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Button, { ButtonProps } from '@material-ui/core/Button';
 
 import { green } from '@material-ui/core/colors';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles<Theme, { fullWidth: boolean }>(theme => ({
   wrapper: {
     margin: theme.spacing(1),
     position: 'relative',
@@ -14,7 +15,7 @@ const useStyles = makeStyles(theme => ({
     position: 'absolute',
     top: '50%',
     left: '50%',
-    marginTop: -6,
+    marginTop: props => (props.fullWidth ? -6 : -12),
     marginLeft: -12,
   },
 }));
@@ -23,12 +24,12 @@ interface LoadingButtonProps extends ButtonProps {
   loading: boolean;
 }
 
-export const LoadingButton: React.FC<LoadingButtonProps> = ({ loading, disabled, children, ...props }) => {
-  const classes = useStyles();
+export const LoadingButton: React.FC<LoadingButtonProps> = ({ loading, disabled, fullWidth, children, ...props }) => {
+  const classes = useStyles({ fullWidth: !!fullWidth });
 
   return (
     <div className={classes.wrapper}>
-      <Button {...props} disabled={disabled || loading}>
+      <Button {...props} disabled={disabled || loading} fullWidth={fullWidth}>
         {children}
       </Button>
       {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
