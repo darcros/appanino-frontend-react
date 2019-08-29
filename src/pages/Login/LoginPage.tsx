adab18f5128b082a926f676bcf531460c4e22cd4
BuildTools <darcros7@gmail.com>
2019-08-29 16:35:39 +0200

    use createStyles

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -3,25 +3,27 @@ import { Redirect } from 'react-router';
 
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
 import { useDoLoginMutation } from '../../generated/graphql';
 import { PageContainer } from '../../components/PageContainer';
 import { LoginForm } from './components/LoginForm';
 
-const useStyles = makeStyles(theme => ({
-  container: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-}));
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+  }),
+);
 
 export const LoginPage: React.FC = () => {
   const classes = useStyles();
