a13f8c56283ebad179532924aa90ae48d0d0fef0
BuildTools <darcros7@gmail.com>
2019-09-04 19:36:17 +0200

    fixed redirect bug by reverting changes

--- src/graphql/client/index.ts
+++ src/graphql/client/index.ts
@@ -1,6 +1,7 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
+import { loggedIn } from './token';
 import { link } from './link';
 import { resolvers } from './resolvers';
 
@@ -8,4 +9,12 @@ export const client = new ApolloClient({
   link,
   cache: new InMemoryCache(),
   resolvers,
+});
+
+// Pre-populate cache.
+client.writeData({
+  data: {
+    // Pre-populating this value resolves the bug that always redirects the user to / when they reload the page.
+    isLoggedIn: loggedIn(),
+  },
 });
