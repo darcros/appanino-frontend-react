ddc4e1f013898fc7d8feb4225b5ad39a59bf16eb
Dario Crosa <darcros7@gmail.com>
2019-10-15 17:26:30 +0200

    implemented signup form

--- src/pages/SignUp/components/SignUpForm.tsx
+++ src/pages/SignUp/components/SignUpForm.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import useForm from 'react-hook-form';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 
+import { useSchoolsQuery } from '../../../generated/graphql';
+import { errorToMessage } from '../../../util/graphql';
+import { getErrorMessage } from '../../../util/form';
 import { LoadingButton } from '../../../components/LoadingButton';
+import { OutlinedSelect } from '../../../components/OutlinedSelect';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -31,29 +38,102 @@ interface SignUpFormProps {
   onSubmit: (formState: SignUpFormState) => void;
 }
 
-export const SignUpForm: React.FC<SignUpFormProps> = ({ loading, errorMessage, onSubmit }) => {
+export const SignUpForm: React.FC<SignUpFormProps> = ({ loading, errorMessage: externalErrorMessage, onSubmit }) => {
   const { t } = useTranslation();
   const classes = useStyles();
+  const { register, handleSubmit, errors } = useForm<SignUpFormState>();
+  const { data, loading: schoolsLoading, error: schoolsError } = useSchoolsQuery();
+  const errorMessage = externalErrorMessage || errorToMessage(schoolsError);
 
   return (
-    <form className={classes.form}>
-      {/* WIP: Fake form just to test the graphql mutation */}
+    <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            autoComplete="fname"
+            name="firstname"
+            variant="outlined"
+            fullWidth
+            id="firstname-textField"
+            label={t('form.first-name.label')}
+            helperText={errors.firstname ? getErrorMessage(errors.firstname) : null}
+            inputRef={register({
+              required: t('form.first-name.required'),
+            })}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            autoComplete="lname"
+            name="lastname"
+            variant="outlined"
+            fullWidth
+            id="lastname-textField"
+            label={t('form.last-name.label')}
+            helperText={errors.lastname ? getErrorMessage(errors.lastname) : null}
+            inputRef={register({
+              required: t('form.last-name.required'),
+            })}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            autoComplete="email"
+            name="email"
+            variant="outlined"
+            fullWidth
+            id="email-textField"
+            label={t('form.email.label')}
+            helperText={errors.email ? getErrorMessage(errors.email) : null}
+            inputRef={register({
+              required: t('form.email.required'),
+            })}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            autoComplete="new-password"
+            name="password"
+            type="password"
+            variant="outlined"
+            fullWidth
+            id="password-textField"
+            label={t('form.password.label')}
+            helperText={errors.password ? getErrorMessage(errors.password) : null}
+            inputRef={register({
+              required: t('form.password.required'),
+            })}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <OutlinedSelect
+            inputName="schoolId"
+            id="schoolId-select"
+            label={t('form.school.label')}
+            inputRef={register}
+            defaultValue={data ? '' : 'LOADING'}
+            disabled={!data}
+          >
+            {data ? (
+              data.schools.map(school => (
+                <option key={school.id} value={school.id}>
+                  {school.name}
+                </option>
+              ))
+            ) : (
+              <option value="LOADING">{t('form.school.loading')}</option>
+            )}
+          </OutlinedSelect>
+        </Grid>
+      </Grid>
+
       <LoadingButton
-        loading={loading}
+        type="submit"
+        fullWidth
         variant="contained"
         color="primary"
-        className={classes.submit}
-        onClick={() =>
-          onSubmit({
-            /* eslint-disable i18next/no-literal-string */
-            firstname: 'John',
-            lastname: 'Doe',
-            email: 'test@example.com',
-            password: 'password',
-            schoolId: '1',
-            /* eslint-enable i18next/no-literal-string */
-          })
-        }
+        loading={loading}
+        disabled={schoolsLoading}
       >
         {t('action.signUp')}
       </LoadingButton>
