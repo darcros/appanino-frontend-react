9515dfb462c58f678cdd28884bdddae13973c3fb
Dario Crosa <darcros7@gmail.com>
2019-11-15 18:03:13 +0100

    removed useless padding

--- src/pages/Cart/CartPage.tsx
+++ src/pages/Cart/CartPage.tsx
@@ -35,26 +35,24 @@ export const CartPage: React.FC = () => {
 
   return (
     <PageContainer maxWidth="md">
-      <Box my={4}>
-        <ProductList products={mappedProducts} />
+      <ProductList products={mappedProducts} />
 
-        <LoadingButton
-          disabled={mappedProducts.length === 0}
-          loading={loading}
-          onClick={placeOrder}
-          fullWidth
-          variant="contained"
-          color="primary"
-        >
-          {t('page.cart.place-order')}
-        </LoadingButton>
+      <LoadingButton
+        disabled={mappedProducts.length === 0}
+        loading={loading}
+        onClick={placeOrder}
+        fullWidth
+        variant="contained"
+        color="primary"
+      >
+        {t('page.cart.place-order')}
+      </LoadingButton>
 
-        {hasError && (
-          <Typography color="error" align="center" component="h3" variant="subtitle2">
-            {t('error.cannot-place-order')}
-          </Typography>
-        )}
-      </Box>
+      {hasError && (
+        <Typography color="error" align="center" component="h3" variant="subtitle2">
+          {t('error.cannot-place-order')}
+        </Typography>
+      )}
     </PageContainer>
   );
 };
