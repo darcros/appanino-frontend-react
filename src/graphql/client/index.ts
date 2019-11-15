49c15f72a36f711af111a0b856a9bdaefc86c6ab
Dario Crosa <darcros7@gmail.com>
2019-11-15 18:19:41 +0100

    removed local schema definition in client

--- src/graphql/client/index.ts
+++ src/graphql/client/index.ts
@@ -1,54 +1,14 @@
-/* eslint-disable i18next/no-literal-string */
-
-import gql from 'graphql-tag';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import { link } from './link';
 import { resolvers, defaults } from './resolvers';
 
-const schema = gql`
-  type CartItem {
-    id: ID!
-    quantity: Int!
-    product: Product!
-  }
-
-  type Cart {
-    # Returns all items that are in cart
-    items: [CartItem!]!
-    # Returns quantity of a product.
-    # Retunrns a quantity of 0 if the product is not in the cart.
-    productQuantity(productId: ID!): Int!
-  }
-
-  extend type Query {
-    cart: Cart!
-  }
-
-  type CartMutations {
-    # Updates the quantity of a certain item in the cart.
-    # If the item did not exist it will be created.
-    updateProductQuantity(productId: ID!, quantity: Int!): Boolean
-  }
-
-  type AuthMutations {
-    saveToken(token: String!): Boolean
-    logOut: Boolean
-  }
-
-  extend type Mutation {
-    cart: CartMutations!
-    auth: AuthMutations!
-  }
-`;
-
 export const client = new ApolloClient({
   link,
   cache: new InMemoryCache(),
   resolvers,
   connectToDevTools: true,
-  typeDefs: schema,
 });
 
 const writeDefaults = () =>
