6b7aea223771f20925d5524984043a89a5f5b02d
Dario Crosa <darcros7@gmail.com>
2019-11-16 20:58:53 +0100

    refactored email update button

--- src/pages/UserSettings/components/UpdateEmailButton.tsx
+++ src/pages/UserSettings/components/UpdateEmailButton.tsx
@@ -1,37 +1,33 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import useForm from 'react-hook-form';
+import * as yup from 'yup';
+import { Formik, Form, Field } from 'formik';
+
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogActions';
-import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
+import { TextField } from 'formik-material-ui';
 
-import { emailRegex, getErrorMessage } from '../../../util/form';
 import { errorToMessage } from '../../../util/graphql';
-import { useDoEmailUpdateMutation, DoEmailUpdateMutationVariables } from '../../../generated/graphql';
+import { useDoEmailUpdateMutation } from '../../../generated/graphql';
 import { LoadingButton } from '../../../components/LoadingButton';
 
 export const UpdateEmailButton: React.FC = () => {
   const { t } = useTranslation();
   const [emailModalOpen, setEmailModalOpen] = React.useState(false);
-  const { register, handleSubmit, reset, errors } = useForm<DoEmailUpdateMutationVariables>();
-  const [doEmailUpdate, { loading, error }] = useDoEmailUpdateMutation();
+  const [doEmailUpdate, { error }] = useDoEmailUpdateMutation();
   const errorMessage = errorToMessage(error);
 
-  const handleClose = (forceClose: boolean = false) => {
-    if (!forceClose && loading) return;
-
-    reset();
-    setEmailModalOpen(false);
-  };
-
-  const onSubmit = (formData: DoEmailUpdateMutationVariables) => {
-    doEmailUpdate({ variables: formData }).then(() => handleClose(true));
-  };
+  const newEmailValidationSchema = yup.object({
+    newEmail: yup
+      .string()
+      .required(t('form.email.required'))
+      .email(t('form.email.invalid')),
+  });
 
   return (
     <React.Fragment>
@@ -39,42 +35,59 @@ export const UpdateEmailButton: React.FC = () => {
         {t('dialog.update-email.button')}
       </Button>
 
-      <Dialog open={emailModalOpen} onClose={() => handleClose()} aria-labelledby="email-update-dialog-title">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <DialogTitle id="email-update-dialog-title">{t('dialog.update-email.title')}</DialogTitle>
-          <DialogContent>
-            <DialogContentText>{t('dialog.update-email.instructions')}</DialogContentText>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              name="newEmail"
-              label={t('dialog.update-email.new-email.label')}
-              autoComplete="email"
-              inputRef={register({
-                required: t('dialog.update-email.new-email.required'),
-                pattern: { value: emailRegex, message: t('form.email.invalid') },
-              })}
-              error={!!errors.newEmail}
-              helperText={errors.newEmail ? getErrorMessage(errors.newEmail) : null}
-              disabled={loading}
-            />
-            {errorMessage && (
-              <Typography color="error" align="center" component="h3" variant="subtitle2">
-                {errorMessage}
-              </Typography>
-            )}
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={() => handleClose()} disabled={loading} color="secondary">
-              {t('action.cancel')}
-            </Button>
-            <LoadingButton loading={loading} type="submit" color="primary">
-              {t('action.confirm')}
-            </LoadingButton>
-          </DialogActions>
-        </form>
-      </Dialog>
+      <Formik
+        initialValues={{
+          newEmail: '',
+        }}
+        validationSchema={newEmailValidationSchema}
+        onSubmit={async values => {
+          await doEmailUpdate({ variables: values });
+          setEmailModalOpen(false);
+        }}
+      >
+        {({ isValidating, isSubmitting, resetForm }) => {
+          const close = () => {
+            if (isValidating || isSubmitting) return;
+            resetForm();
+            setEmailModalOpen(false);
+          };
+
+          return (
+            <Dialog open={emailModalOpen} onClose={() => close()} aria-labelledby="email-update-dialog-title">
+              <Form>
+                <DialogTitle id="email-update-dialog-title">{t('dialog.update-email.title')}</DialogTitle>
+                <DialogContent>
+                  <DialogContentText>{t('dialog.update-email.instructions')}</DialogContentText>
+                  <Field
+                    name="newEmail"
+                    component={TextField}
+                    disabled={isValidating || isSubmitting}
+                    label={t('dialog.update-email.new-email.label')}
+                    type="email"
+                    autoComplete="email"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                  />
+                  {errorMessage && (
+                    <Typography color="error" align="center" component="h3" variant="subtitle2">
+                      {errorMessage}
+                    </Typography>
+                  )}
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={() => close()} disabled={isValidating || isSubmitting} color="secondary">
+                    {t('action.cancel')}
+                  </Button>
+                  <LoadingButton loading={isValidating || isSubmitting} type="submit" color="primary">
+                    {t('action.confirm')}
+                  </LoadingButton>
+                </DialogActions>
+              </Form>
+            </Dialog>
+          );
+        }}
+      </Formik>
     </React.Fragment>
   );
 };
